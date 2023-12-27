338022ad605cd9c8632f5562da6be778cbe6261b
KasamaLee <k.kasama.lee@gmail.com>
2023-12-27 12:26:17 +0700

    update delete

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -19,3 +19,60 @@ exports.createTask = async (req, res, next) => {
 
     }
 }
+
+exports.readTask = async (req, res, next) => {
+    try {
+        const allTask = await prisma.task.findMany()
+        res.status(200).json({ allTask })
+    } catch (err) {
+        console.log(err)
+        next(err)
+    }
+}
+
+exports.updateTask = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { text } = req.body;
+
+        const updatedTask = await prisma.task.update({
+            where: {
+                id: +id
+            },
+            data: {
+                text: text
+            }
+        })
+
+        res.status(200).json({ updatedTask })
+    } catch (err) {
+        console.log(err)
+        next(err)
+    }
+}
+
+exports.deleteTask = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+
+        const deletedTask = await prisma.task.delete({
+            where: {
+                id: +id
+            }
+        })
+        res.status(200).json({ deletedTask })
+    } catch (err) {
+        console.log(err)
+        next(err)
+    }
+}
+
+exports.updateTaskStatus = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        console.log(id)
+    } catch (err) {
+        console.log(err)
+        next(err)
+    }
+}
--- src/routes/todoRoute.js
+++ src/routes/todoRoute.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const todoController = require('../controllers/todoController')
 
 router.post('/create', todoController.createTask)
+router.get('/read', todoController.readTask)
+router.patch('/update/:id', todoController.updateTask)
+router.delete('/delete/:id', todoController.deleteTask)
 
 module.exports = router;
